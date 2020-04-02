63b722a92e0c02ab8c9128dc6025b49bcf643d23
greg-mich <madpeeps@yahoo.com>
2020-04-02 00:48:55 -0500

    started work on first entity, and app db context, no migrations or entity configuration yet with ef core

---
+++ Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fitness_Tracker.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness_Tracker.Data.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base (options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
