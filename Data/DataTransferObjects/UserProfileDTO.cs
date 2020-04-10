1fced8c2e469043670975edff03f0a9c02eec146
greg-mich <gregmich95@gmail.com>
2020-04-09 21:27:59 -0500

    implemented user profile controller, added logging, added userProfile query object and DTO

---
+++ Data/DataTransferObjects/UserProfileDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker.Data.DataTransferObjects
+{
+    public class UserProfileDTO
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ Data/QueryObjects/UserProfileQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fitness_Tracker.Data.Contexts;
+using Fitness_Tracker.Data.Entities;
+using Fitness_Tracker.Data.DataTransferObjects;
+
+namespace Fitness_Tracker.Data.QueryObjects
+{
+    public static class UserProfileQuery
+    {
+        public static IQueryable<UserProfileDTO> GetUserProfile(
+            this IQueryable<User> users) =>
+                users
+                .Select(user => new UserProfileDTO
+                {
+                    UserId = user.UserId,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.Username,
+                    Email = user.Email,
+                    BirthDate = user.BirthDate
+                });
+    }
+}
